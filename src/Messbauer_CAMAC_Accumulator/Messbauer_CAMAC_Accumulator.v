8faefefbe9942b83319d69de7bf84de9aa7ad6f6
Ushakov Michael <um.nix.user@gmail.com>
2023-02-18 12:51:17 +0500

    Сохранение накопленного значения в спектр, сброс счетчиков

--- src/Messbauer_CAMAC_Accumulator/Messbauer_CAMAC_Accumulator.v
+++ src/Messbauer_CAMAC_Accumulator/Messbauer_CAMAC_Accumulator.v
@@ -66,6 +66,7 @@ localparam reg [CAMAC_FUNC_WIDTH-1:0] AMPLITUDE_MODE_FUNC = 24;
 localparam reg [CAMAC_FUNC_WIDTH-1:0] AUTONOMOUS_MODE_FUNC = 26;
 localparam reg [1:0] AMPLITUDE_MODE = 1;
 localparam reg [1:0] AUTONOMOUS_MODE = 2;
+localparam reg [MESSB_ACC_ADDRESS_WIDTH-1:0] LAST_ADDRESS = 2**MESSB_ACC_ADDRESS_WIDTH - 1;
 /*********************************************************************************/
 /******************************* Блок переменных *********************************/
 reg [3:0] state;                           // состояние блока накопления. см. диаграмму
@@ -74,17 +75,17 @@ reg [1:0] mode;                            // режим измерения
 reg start_trigger;                         // триггер старт-импульса
 reg [MESSB_ACC_ADDRESS_WIDTH-1:0] address; // адрес текущей точки спектра
 reg current_counter;                       // текущий используемый счетчик
-//reg [CAMAC_DATA_WIDTH-1:0] current_value;  // текущее значение
+reg [CAMAC_DATA_WIDTH-1:0] current_value;  // текущее значение
 reg [CAMAC_DATA_WIDTH-1:0] counter1_value; // текущее значение накопленное в счетчике 1
 reg [CAMAC_DATA_WIDTH-1:0] counter2_value; // текущее значение накопленное в счетчике 2
 reg channel_switched;                      // триггер переключения канал-импульса
 // специальный синтаксис 2 ** MESSB_ACC_ADDRESS_WIDTH = 2 в степени
 reg [CAMAC_DATA_WIDTH-1:0] spectrum [2**MESSB_ACC_ADDRESS_WIDTH];
 reg channel_data_accumulated;              // регистр, используемый для 
-wire channel_trigger_rst;                  // эффективный сигнал сброса триггера канала
+wire acc_event_rst;                        // эффективный сигнал сброса триггера канала и очистки счетсчика импульсов
 integer i;                                 // Переменная для цикла for для инициализации спектра
 /*********************************************************************************/
-assign channel_trigger_rst = rst & channel_data_accumulated;
+assign acc_event_rst = rst | channel_data_accumulated;
 /****************** Блок описания поведения работы накопителя ********************/
 // Блок логики смены состояний
 always @(posedge clk)
@@ -96,6 +97,7 @@ begin
         // todo(UMV) : добавить инициализацию остальных регистров
         address <= 0;
         channel_data_accumulated <= 1'b0;
+        current_value <= 0;
         for (i = 0; i < 2 ** MESSB_ACC_ADDRESS_WIDTH; i = i+1)
         begin
             spectrum[i] <= 0;
@@ -145,7 +147,7 @@ begin
             begin
                 if (start_trigger == 1'b1)
                 begin
-                    // инициализация
+                    // запуск накопления от 0 до 2^MESSB_ACC_ADDRESS_WIDTH - 1 точки
                     address <= 0;
                     state <= MESSB_ACC_ACCUMULATION_ADDR_SEL_STATE;
                 end
@@ -158,6 +160,7 @@ begin
             MESSB_ACC_ACCUMULATION_COUNTER1_COUNT_STATE:
             begin
                 current_counter <= 1'b0;
+                channel_data_accumulated <= 1'b0;
                 // ждем окончания канала ...
                 // по завершении копируем в блок памяти в ПЛИС, переходим в MESSB_ACC_ACCUMULATION_VALUE_COPY_STATE
                 if (channel_switched == 1'b1)
@@ -168,6 +171,7 @@ begin
             MESSB_ACC_ACCUMULATION_COUNTER2_COUNT_STATE:
             begin
                 current_counter <= 1'b1;
+                channel_data_accumulated <= 1'b0;
                 // ждем окончания канала ...
                 // по завершении копируем в блок памяти в ПЛИС, переходим в MESSB_ACC_ACCUMULATION_VALUE_COPY_STATE
                 if (channel_switched == 1'b1)
@@ -180,15 +184,28 @@ begin
                здесь осуществляется перенос значения в ячейку памяти -> s[i] = s[i] + value
             */
             begin
-                address <= address + 1;
-                
+                if (current_counter == 1'b0)
+                    current_value <= counter1_value;
+                else
+                    current_value <= counter2_value;
+                spectrum[address] = spectrum[address] + current_value;
                 channel_data_accumulated <= 1'b1;
+                if (address == LAST_ADDRESS)
+                begin
+                    state <= MESSB_ACC_ACCUMULATION_CYCLE_FINISHED_STATE;
+                end
+                else
+                begin
+                    address <= address + 1;
+                    state <= MESSB_ACC_ACCUMULATION_ADDR_SEL_STATE;
+                end
             end
             MESSB_ACC_ACCUMULATION_CYCLE_FINISHED_STATE:
             begin
             /* Последовательно были измерены все точки спектра от 0 до 2^12-1 (4095)
-             * В этом состоянии очищаем все временные значения если необходимо
+             * В этом состоянии очищаем все временные значения если необходимо, готовимся к следующему циклу
              */
+            state <= MESSB_ACC_ACCUMULATION_CYCLE_STARTED_STATE;
             end
         endcase
     end
@@ -204,15 +221,27 @@ begin
         start_trigger <= 1'b0;
 end
 
-// Блок подсчета импульсов
-always @(posedge count or posedge rst)
+// Блок подсчета импульсов с асинхронным сбросом
+always @(posedge count or posedge acc_event_rst)
 begin
     // асинхронный сброс для задания начального значения для current_value
-    if (rst == 1'b1)
+    if (acc_event_rst == 1'b1)
     begin
-        // тут нужен "хитрый сброс"
-        counter1_value <= 0;
-        counter2_value <= 0;
+        // тут нужен "хитрый сброс",т.е. когда идет счет и счет происходит в сч.1, сбросить можно
+        // если для сч.2 данные уже попали в память аналогично и с другим счетчиком
+        if (rst == 1'b1)
+        begin
+            // это глобальный сброс, мы очищаем оба счетчика
+            counter1_value <= 0;
+            counter2_value <= 0;
+        end
+        else
+        begin
+            if (current_counter == 1'b0)
+                counter2_value <= 0;
+            else
+                counter1_value <= 0;
+        end
     end
     else
     begin
@@ -223,14 +252,13 @@ begin
     end
 end
 
-/* Блок смены адреса точки спектра, переключение из 1 в 0 обозначает начало накопления
- * в новую ячейку
+/* Блок смены адреса точки спектра с асинхронным сбросом
  * start   -----_-----------------------------------------------------------.....------_----....
  * channel -----__----------------__----------------__-----------------__---.....------__---....
  */
-always @(negedge channel or posedge channel_trigger_rst)
+always @(negedge channel or posedge acc_event_rst)
 begin
-if (channel_trigger_rst == 1'b1)
+if (acc_event_rst == 1'b1)
     begin
         channel_switched <= 1'b0;
     end
