e997e7d890683d4b6fa05f37a6a6f32d818d5a8e
Ushakov Michael <um.nix.user@gmail.com>
2023-02-25 23:32:42 +0500

    Полностью описаны все сигналы: CAMAC && RS232

--- src/Messbauer_CAMAC_Controller/Messbauer_CAMAC_Controller.v
+++ src/Messbauer_CAMAC_Controller/Messbauer_CAMAC_Controller.v
@@ -20,27 +20,42 @@
 module Messbauer_CAMAC_Controller #
 (
     CAMAC_DATA_WIDTH = 24,
+    CAMAC_MODULE_WIDTH = 6,
     CAMAC_FUNC_WIDTH = 5,
-    MESSB_ACC_ADDRESS_WIDTH = 12
+    CAMAC_SUB_ADDR_WIDTH = 4
 )
 (
     // Общие сигналы управления: тактовая частота и чигнал сброса
     input wire clk,
     input wire rst,
-
+    
+    // RS232-сигналы
+    input wire rs232_rx,                             // сигнал RX чтение данных PC -> устройство (контроллер крейта)
+    output wire rs232_tx,                            // сигнал TX чтение данных PC <- устройство (контроллер крейта)
+    input wire rs232_cts                             // сигнал Clear-to-send, PC выставляет когда готов принять данные
+    output wire rs232_rts,                           // сигнал Ready-to-send, устройство выставляет когда готово отправить данные
 
     // CAMAC-сигналы
-    // Сигналы управления адресацией юю
-    input [CAMAC_FUNC_WIDTH-1:0] camac_f,
-
-    input camac_s1,
-    output reg [CAMAC_DATA_WIDTH-1:0] read,  // should be input && rename
-    output reg [CAMAC_DATA_WIDTH-1:0] write,
-    output reg camac_x,
-    output reg camac_q,
-    
-
-    output reg trig  // ?
+    // В CAMAC инверсная по отношению к стандарту ТТЛ логика (т.е. лог. 0 CAMAC == лог. 1 ТТЛ)
+    // Сигналы управления адресацией модулей (N, F, A)
+    output wire [CAMAC_MODULE_WIDTH-1:0] camac_n,    // выбор модуля
+    output wire [CAMAC_FUNC_WIDTH-1:0] camac_f,      // выбор функции
+    output wire [CAMAC_SUB_ADDR_WIDTH-1:0] camac_a,  // выбор суб адреса
+    // Сигналы состояния
+    input wire camac_x,                              // сигнал команда принята, модуль -> контроллер на любой NAF
+    input wire camac_q,                              // сигнал ответ, модуль -> контроллер на любой NAF
+    input wire camac_l,                              // сигнал запрос на обслуживание, модуль -> контроллер
+    output wire camac_b,                             // сигнал занято, контроллер -> магистраль, вырабатывается при любой операции контроллером
+    // Сигналы управления
+    output wire camac_z,                             // сигнал начальная установка (= Пуск), контроллер -> магистраль
+    output wire camac_c,                             // сигнал сброс, контроллер -> магистраль
+    output wire camac_i,                             // сигнал запрет, контроллер -> магистраль/устройство
+    output wire camac_s1,                            // сигнал строб S1, контроллер -> магистраль
+    output wire camac_s12,                           // сигнал строб S2, контроллер -> магистраль
+    output wire camac_h,                             // сигнал задержка, контроллер -> магистраль/устройство (нестандартный сигнал)
+    // Сигналы передачи данных
+    input wire [CAMAC_DATA_WIDTH-1:0] camac_r,  // should be input && rename
+    output wire [CAMAC_DATA_WIDTH-1:0] camac_w
 );
 
 
