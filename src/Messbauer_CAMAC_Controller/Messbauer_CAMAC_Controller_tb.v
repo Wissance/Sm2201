291fd3b79e9f3b2b617c2cd4ff37db8394e07902
Ushakov Michale <um.nix.user@gmail.com>
2024-10-02 18:32:24 +0500

    added empty tb

---
+++ src/Messbauer_CAMAC_Controller/Messbauer_CAMAC_Controller_tb.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:             Wissance (https://wissance.com)
+// Engineer:            EvilLord666 (Ushakov MV - https://github.com/EvilLord666)
+// 
+// Create Date:         29.06.2023 
+// Design Name:         SerialCmdProcessor
+// Module Name:         serial_cmd_decoder_tb
+// Project Name:        SerialCmdProcessor
+// Target Devices:      QMTECH CycloneIV Core Board (EP4CE15F23C8N)
+// Tool versions:       Quartus Prime Lite 18.1
+// Description:         A Testbench for testing Messbauer_CAMAC_Controller
+//
+// Dependencies:        Depends on Messbauer_CAMAC_Controller
+//
+// Revision:            1.0
+// Additional Comments: A minimal set of tests
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module Messbauer_CAMAC_Controller_tb();
+
+Messbauer_CAMAC_Controller camac_controller();
+
+endmodule
